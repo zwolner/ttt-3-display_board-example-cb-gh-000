c0ca97c7ebbf2aea2d8076c0b677e43d19ccdc9a
Zachary Wolner <zachary.wolner@gmail.com>
2017-12-14 21:06:59 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,6 +1,6 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
 def display_board
-print "   |   |   
+print "   |   |
 -----------
    |   |   
 -----------
@@ -8,7 +8,7 @@ print "   |   |
 
 "   |   |
 -----------
-   |   |   
+   |   |
 -----------
    |   |   "
 
